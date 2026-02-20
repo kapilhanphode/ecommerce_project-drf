f0164f3d6fa52f0fc1198f9d36cecfad1ee06b59
Kapil <kapilhanphode11@gmail.com>
2026-02-20 12:11:15 +0530

    for many = true

--- store/api/serializers.py
+++ store/api/serializers.py
@@ -1,22 +1,28 @@
 from rest_framework import serializers
 from store.models import Product, Category, ProductImage
 
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'color']
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name', 'slug')
 
+
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
+    images = ProductImageSerializer(many=True)
 
     class Meta:
         model = Product
-        fields = ["id", "name", "description", "price", "category"]
+        fields = ["id", "name", "description", "price", "category", "images"]
 
     def create(self, validated_data):
         category = validated_data.pop('category')
-        # images = validated_data.pop('images')
+        images = validated_data.pop('images')
 
         category, created = Category.objects.get_or_create(
             slug=category.get('slug'),
@@ -27,11 +33,26 @@ class ProductSerializer(serializers.ModelSerializer):
             category=category,
             **validated_data
         )
+        for image in images:
+            ProductImage.objects.create(product=product, **image)
         return product
 
+    # def update(self, instance, validated_data):
+    #     images = validated_data.pop('images')
+    #     for attr, value in validated_data.items():
+    #         print('instance...............................', instance)
+    #         print('attr and value.........................', attr, value)
+    #         setattr(instance, attr, value)
+    #     instance.save()
+    #     if images is not None:
+    #         instance.images.all().delete()
+    #         for image in images:
+    #             ProductImage.objects.create(product=instance, **image)
+    #     return instance
 
     def update(self, instance, validated_data):
         category = validated_data.pop('category')
+        images = validated_data.pop('images')
 
         instance.name = validated_data.get('name', instance.name)
         instance.price = validated_data.get('price', instance.price)
@@ -42,5 +63,8 @@ class ProductSerializer(serializers.ModelSerializer):
                 defaults={'name': category.get('name')}
             )
             instance.category = category
+        for image in images:
+            ProductImage.objects.create(product=instance, **image)
+
         instance.save()
         return instance
